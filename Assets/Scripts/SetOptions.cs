770232b46383f64e5e87985dc50d707903c48372
Tim Graupmann <tgraupmann@gmail.com>
2017-01-19 12:16:35 -0800

    Initial commit of files

---
+++ Assets/Scripts/SetOptions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetOptions : MonoBehaviour
+{
+    public Dropdown _mDropDown = null;
+
+    Dropdown.OptionData CreateOptionData(string text)
+    {
+        Dropdown.OptionData data = new Dropdown.OptionData();
+        data.text = text;
+        return data;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        if (_mDropDown)
+        {
+            List<Dropdown.OptionData> options = new List<Dropdown.OptionData>();
+            options.Add(CreateOptionData("Languages"));
+            options.Add(CreateOptionData("Something else"));
+            _mDropDown.ClearOptions();
+            _mDropDown.AddOptions(options);
+        }
+    }
+}
