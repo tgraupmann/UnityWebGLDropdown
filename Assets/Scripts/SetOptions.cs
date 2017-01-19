c3ebd08d017ec79f42798ddc07a520442868dc75
Tim Graupmann <tgraupmann@gmail.com>
2017-01-19 13:19:21 -0800

    Added displaying special characters

--- Assets/Scripts/SetOptions.cs
+++ Assets/Scripts/SetOptions.cs
@@ -1,10 +1,33 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SetOptions : MonoBehaviour
 {
     public Dropdown _mDropDown = null;
+
+    public TextAsset _mTextAsset = null;
+
+    [Serializable]
+    public class LanguageResult
+    {
+        public Language[] languages;
+    }
+
+    [Serializable]
+    public class Language
+    {
+        public string name = string.Empty;
+        public Dialect[] dialects;
+    }
+
+    [Serializable]
+    public class Dialect
+    {
+        public string name = string.Empty;
+        public string description = string.Empty;
+    }
 
     Dropdown.OptionData CreateOptionData(string text)
     {
@@ -16,11 +39,16 @@ public class SetOptions : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        if (_mDropDown)
+        if (_mDropDown &&
+            _mTextAsset)
         {
+            LanguageResult result = JsonUtility.FromJson<LanguageResult>(_mTextAsset.text);
             List<Dropdown.OptionData> options = new List<Dropdown.OptionData>();
             options.Add(CreateOptionData("Languages"));
-            options.Add(CreateOptionData("Something else"));
+            foreach (Language language in result.languages)
+            {
+                options.Add(CreateOptionData(language.name));
+            }
             _mDropDown.ClearOptions();
             _mDropDown.AddOptions(options);
         }
